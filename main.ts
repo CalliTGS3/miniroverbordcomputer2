e24c9d8098d10c0b1df79e6e7ba5e51f32fb15db
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2023-03-06 11:50:06 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -32,9 +32,7 @@ basic.forever(function () {
     if (Fahren && input.runningTime() < ZeitstempelEmpfangen + Timeout) {
         basic.setLedColor(0x00ff00)
         basic.clearScreen()
-        LedX = Math.map(LedX, 0, 4, 4, 0)
-        LedY = Math.map(LedY, 0, 4, 4, 0)
-        led.plot(LedX, LedY)
+        led.plot(4 - LedX, 4 - LedY)
         serial.writeValue("L", MotorLinks)
         serial.writeValue("R", MotorRechts)
         motors.dualMotorPower(Motor.A, MotorLinks)
