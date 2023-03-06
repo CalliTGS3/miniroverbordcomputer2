cc2edcadeee4cef6863fa786f30887cdbe09a03e
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2023-03-06 10:26:49 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,45 @@
+radio.onReceivedValue(function (name, value) {
+    ZeitstempelEmpfangen = input.runningTime()
+    if (name == "F") {
+        if (value == 1) {
+            Fahren = true
+        } else {
+            Fahren = false
+        }
+    } else if (name == "L") {
+        MotorLinks = value
+    } else if (name == "R") {
+        MotorRechts = value
+    } else if (name == "X") {
+        LedX = value
+    } else if (name == "Y") {
+        LedY = value
+    } else {
+    	
+    }
+})
+let LedY = 0
+let LedX = 0
+let MotorRechts = 0
+let MotorLinks = 0
+let Fahren = false
+let ZeitstempelEmpfangen = 0
+let Timeout = 500
+radio.setGroup(1)
+basic.setLedColor(0x007fff)
+serial.redirectToUSB()
+basic.forever(function () {
+    if (Fahren && input.runningTime() < ZeitstempelEmpfangen + Timeout) {
+        basic.setLedColor(0x00ff00)
+        basic.clearScreen()
+        led.plot(LedX, LedY)
+        serial.writeValue("L", MotorLinks)
+        serial.writeValue("R", MotorRechts)
+        motors.dualMotorPower(Motor.A, MotorLinks)
+        motors.dualMotorPower(Motor.B, MotorRechts)
+    } else {
+        basic.setLedColor(0x007fff)
+        motors.dualMotorPower(Motor.A, 0)
+        motors.dualMotorPower(Motor.B, 0)
+    }
+})
